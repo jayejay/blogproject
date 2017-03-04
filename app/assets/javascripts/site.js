7881b61ed35637ed87ffd2a53dd9347465fb2c3d
JayUbuntu <julianeissing@web.de>
2017-03-04 19:10:22 +0100

    - integrating tinymce

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,10 +2,12 @@ $(document).on('turbolinks:load', function () {
 
     tinymce.init({
         selector: '.textarea',
-        plugins : 'advlist autolink link image lists charmap print preview',
-
+        plugins : 'advlist autolink link image lists charmap print preview imagetools textcolor colorpicker',
+        height: "500px",
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify' +
+        ' | bullist numlist outdent indent | link image',
+        toolbar2: "forecolor | backcolor"
     });
-
 })
 
 
