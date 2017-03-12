ebe47c4509bad3b29e084ba7ccd893e7104aa5ff
JayUbuntu <julianeissing@web.de>
2017-03-12 21:24:46 +0100

    - including layout

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#welcome'
 
+  get 'static_pages/welcome'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
