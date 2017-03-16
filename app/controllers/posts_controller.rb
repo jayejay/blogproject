9d64561ac42711b8b92e76f1d118d96be8c7d8f1
JayUbuntu <julianeissing@web.de>
2017-03-16 19:27:04 +0100

    - inserted sections to links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.where(section_id: params[:section_id])
   end
 
   # GET /posts/1
