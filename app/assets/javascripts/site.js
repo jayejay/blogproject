a5689025a4ee12c8faa856f5a91cc9ca59b77aeb
JayUbuntu <julianeissing@web.de>
2017-03-25 11:17:39 +0100

    -  using tinymce-rails, tinymce-rails-imageupload and carrierwave for uploading images to aws - changed background-image

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,11 +2,11 @@ $(document).on('turbolinks:load', function () {
 
     // tinymce.init({
     //     selector: '.textarea',
-    //     plugins : 'advlist autolink link image lists charmap print preview imagetools textcolor colorpicker',
+    //     plugins : 'advlist autolink link image lists charmap print preview imagetools textcolor colorpicker uploadimage',
     //     height: "500px",
     //     toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify' +
     //     ' | bullist numlist outdent indent | link image',
-    //     toolbar2: "forecolor | backcolor",
+    //     toolbar2: "forecolor | backcolor | uploadimage",
     //     paste_data_images: true
     // });
 
