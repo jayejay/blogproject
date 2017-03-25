5621d8a5f95dd5bfb6aa95d4f2cba5f939dd4b84
JayUbuntu <julianeissing@web.de>
2017-03-25 12:38:01 +0100

    -  going on with tinymce-rails

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -9,17 +9,17 @@ $(document).on('turbolinks:load', function () {
     //     toolbar2: "forecolor | backcolor | uploadimage",
     //     paste_data_images: true
     // });
-
-    $(function() {
-        $('.textarea').tinymce({
-            plugins : 'advlist autolink link image lists charmap print preview imagetools textcolor colorpicker uploadimage',
-            height: "500px",
-            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify' +
-            ' | bullist numlist outdent indent | link image',
-            toolbar2: "forecolor | backcolor | uploadimage",
-            paste_data_images: true
-        });
-    });
+    //
+    // $(function() {
+    //     $('.textarea').tinymce({
+    //         plugins : 'advlist autolink link image lists charmap print preview imagetools textcolor colorpicker uploadimage',
+    //         height: "500px",
+    //         toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify' +
+    //         ' | bullist numlist outdent indent | link image',
+    //         toolbar2: "forecolor | backcolor | uploadimage",
+    //         paste_data_images: true
+    //     });
+    // });
 
     $('.alert').delay(2000).fadeOut(1500);
 
