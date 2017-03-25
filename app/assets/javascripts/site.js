21164c32d2d4866b8ef0ebd0ff9004303ac9db16
JayUbuntu <julianeissing@web.de>
2017-03-25 11:54:13 +0100

    -  testing without tinymce-rails

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,15 +1,24 @@
 $(document).on('turbolinks:load', function () {
 
-    tinymce.init({
-        selector: '.textarea',
-        plugins : 'advlist autolink link image lists charmap print preview imagetools textcolor colorpicker uploadimage',
-        height: "500px",
-        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify' +
-        ' | bullist numlist outdent indent | link image',
-        toolbar2: "forecolor | backcolor | uploadimage",
-        paste_data_images: true
+    // tinymce.init({
+    //     selector: '.textarea',
+    //     plugins : 'advlist autolink link image lists charmap print preview imagetools textcolor colorpicker uploadimage',
+    //     height: "500px",
+    //     toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify' +
+    //     ' | bullist numlist outdent indent | link image',
+    //     toolbar2: "forecolor | backcolor | uploadimage",
+    //     paste_data_images: true
+    // });
+    $(function() {
+        $('textarea.tinymce').tinymce({
+            plugins : 'advlist autolink link image lists charmap print preview imagetools textcolor colorpicker uploadimage',
+            height: "500px",
+            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify' +
+            ' | bullist numlist outdent indent | link image',
+            toolbar2: "forecolor | backcolor | uploadimage",
+            paste_data_images: true
+        });
     });
-
 
     $('.alert').delay(2000).fadeOut(1500);
 
