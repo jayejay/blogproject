39e1596e163437be097c9cd90cbe56d598831005
JayUbuntu <julianeissing@web.de>
2017-04-02 19:53:07 +0200

    - worked on creating user template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action   :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
-  load_and_authorize_resource
+  #before_action :authenticate_user!
+  #load_and_authorize_resource
 
   # GET /users
   # GET /users.json
@@ -71,6 +71,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :nick_name, :admin, :moderator)
+      params.require(:user).permit(:first_name, :last_name, :nick_name, :admin, :moderator,\
+        :email, :password, :password_confirmation)
     end
 end
