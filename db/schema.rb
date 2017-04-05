1db39600d4ebf83190b12f68be23af1ef2960f78
JayUbuntu <julianeissing@web.de>
2017-04-05 19:25:39 +0200

    - added colored tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402180114) do
+ActiveRecord::Schema.define(version: 20170405163653) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20170402180114) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "color"
   end
 
   create_table "users", force: :cascade do |t|
