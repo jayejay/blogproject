46204ae645c9b4d9b7a6d4da7709c73d2e05571c
JayUbuntu <julianeissing@web.de>
2017-04-09 17:33:17 +0200

    - fixed problem with published_at

---
+++ db/migrate/20170409151329_change_column_published_at_from_posts.rb
@@ -0,0 +1,5 @@
+class ChangeColumnPublishedAtFromPosts < ActiveRecord::Migration[5.0]
+  def change
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408160952) do
+ActiveRecord::Schema.define(version: 20170409151329) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
