208375fb715fd9e2740bb7e9e9e0d90e53bc2d60
JayUbuntu <julianeissing@web.de>
2017-04-21 09:47:09 +0200

    - fixed facebook issue

---
+++ app/assets/javascripts/facebook.js
@@ -0,0 +1,39 @@
+// FacebookSDK
+// https://developers.facebook.com/docs/plugins/page-plugin/
+
+/*Facebook likes*/
+ (function(d, s, id) {
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) return;
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.8&appId=326019507801319";
+ fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+// Compatibility with Turbolinks 5
+(function($) {
+  var fbRoot;
+
+  function saveFacebookRoot() {
+    if ($('#fb-root').length) {
+      fbRoot = $('#fb-root').detach();
+}
+};
+
+function restoreFacebookRoot() {
+  if (fbRoot != null) {
+    if ($('#fb-root').length) {
+      $('#fb-root').replaceWith(fbRoot);
+} else {
+  $('body').append(fbRoot);
+}
+}
+
+if (typeof FB !== "undefined" && FB !== null) { // Instance of FacebookSDK
+  FB.XFBML.parse();
+}
+};
+
+document.addEventListener('turbolinks:request-start', saveFacebookRoot)
+document.addEventListener('turbolinks:load', restoreFacebookRoot)
+}(jQuery));
