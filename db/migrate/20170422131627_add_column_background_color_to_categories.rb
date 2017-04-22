21c22d1e95bfa54dff64c4660dd4423e34b1c438
JayUbuntu <julianeissing@web.de>
2017-04-22 15:39:00 +0200

    - post-category-indexes with different colors

---
+++ db/migrate/20170422131627_add_column_background_color_to_categories.rb
@@ -0,0 +1,5 @@
+class AddColumnBackgroundColorToCategories < ActiveRecord::Migration[5.0]
+  def change
+    add_column :categories, :background_color, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416133016) do
+ActiveRecord::Schema.define(version: 20170422131627) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.string "background_color"
   end
 
   create_table "comments", force: :cascade do |t|
